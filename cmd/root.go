3c09193ad8fdba30a8a1f50b78813b0ee06079f2
EscerWF <mosu72@gmail.com>
2021-07-11 17:57:44 +0900

    Added support for saving base64 images.

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,12 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"mime"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
 	"github.com/spf13/cobra"
@@ -26,39 +30,167 @@ var (
 	}
 )
 
-func basicAuth(username, password string) string {
-	auth := username + ":" + password
+type imageUrl struct {
+	url string
+}
+
+func (iu *imageUrl) __init() {
+	pattUrl := regexp.MustCompile(` [0-9]*[wx]?$`)
+	iu.url = pattUrl.ReplaceAllString(iu.url, "")
+	iu.url = strings.TrimSpace(iu.url)
+}
+
+func newImageUrl(url string) *imageUrl {
+	iu := &imageUrl{url: url}
+	iu.__init()
+	return iu
+}
+
+type localImage struct {
+	basename    string
+	contentType string
+}
+
+func (iu *localImage) __init() {
+	ext, _ := mime.ExtensionsByType(iu.contentType)
+	iu.basename = colly.SanitizeFileName(iu.basename)
+
+	if len(ext) > 0 {
+		pattUrl := regexp.MustCompile(`.unknown$`)
+		iu.basename = pattUrl.ReplaceAllString(iu.basename, ext[0])
+	}
+}
+
+func newlocalImage(basename string, contentType string) *localImage {
+	li := &localImage{
+		basename:    basename,
+		contentType: contentType,
+	}
+	li.__init()
+	return li
+}
+
+func imageUrls(e *colly.HTMLElement) *[]imageUrl {
+	var urls []imageUrl
+	src := e.Attr("src")
+	urls = append(urls, *newImageUrl(src))
+
+	srcset := e.Attr("srcset")
+	srcsets := strings.Split(srcset, ",")
+
+	for _, url := range srcsets {
+		urls = append(urls, *newImageUrl(url))
+	}
+
+	return &urls
+}
+
+func basicAuth(user, pass string) string {
+	auth := user + ":" + pass
 	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
 func mainRun(cmd *cobra.Command, args []string) {
+	var seq = 0
+
+	userAgent := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+		"AppleWebKit/537.36 (KHTML, like Gecko)",
+		"Chrome/91.0.4472.124 Safari/537.36",
+	}
+
 	c := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+		colly.UserAgent(strings.Join(userAgent, " ")),
 	)
 
 	hdr := http.Header{}
 	hdr.Set("Authorization", "Basic "+basicAuth("test", "test"))
 
-	c.OnHTML("img", func(e *colly.HTMLElement) {
-		link := e.Attr("src")
-		log.Println(e.Request.AbsoluteURL(link))
-		c.Visit(e.Request.AbsoluteURL(link))
+	c.OnHTML("img, source", func(e *colly.HTMLElement) {
+		for _, url := range *imageUrls(e) {
+			c.Visit(e.Request.AbsoluteURL(url.url))
+		}
+	})
+
+	c.OnHTML("svg", func(e *colly.HTMLElement) {
+		// link := e.Attr("src")
+		// link = strings.TrimSpace(link)
+		// c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnHTML("*[style]", func(e *colly.HTMLElement) {
+		style := e.Attr("style")
+		pattUrl := regexp.MustCompile(`.*background-image:url\((.*)\).*`)
+		styleImage := pattUrl.FindStringSubmatch(style)
+		if styleImage == nil {
+			return
+		}
+
+		iu := newImageUrl(styleImage[0])
+		c.Visit(e.Request.AbsoluteURL(iu.url))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		pattBs64 := regexp.MustCompile(`image\/([\S\D]+);base64,`)
+		if isBs64 := pattBs64.MatchString(r.URL.Opaque); isBs64 {
+			r.Abort()
+
+			var extention = "unknown"
+			contentType := "image/" + pattBs64.FindStringSubmatch(r.URL.Opaque)[1]
+			exts, _ := mime.ExtensionsByType(contentType)
+			if len(exts) > 0 {
+				extention = exts[0]
+			}
+
+			bs64 := pattBs64.ReplaceAllString(r.URL.Opaque, "")
+			dec, err := base64.StdEncoding.DecodeString(bs64)
+			if err != nil {
+				log.Println(err.Error())
+			}
+
+			seq += 1
+			f, err := os.Create("base64Image_" + strconv.Itoa(seq) + "." + extention)
+			if err != nil {
+				log.Println(err.Error())
+			}
+			defer f.Close()
+
+			_, err = f.Write(dec)
+			if err != nil {
+				log.Println(err.Error())
+			}
+			err = f.Sync()
+			if err != nil {
+				log.Println(err.Error())
+			}
+		}
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		cleanFName := colly.SanitizeFileName(r.FileName())
-		r.Save("./" + cleanFName)
-	})
+		contentType := r.Headers.Get("content-type")
+		if isImage := strings.Contains(contentType, "image/"); !isImage {
+			return
+		}
 
-	c.OnResponse(func(r *colly.Response) {
+		li := newlocalImage(r.FileName(), contentType)
+		err := r.Save("./" + li.basename)
+		if err != nil {
+			log.Println(err.Error())
+		}
 		// for debugging
-		log.Println("response received", r.StatusCode)
+		log.Println("response url", r.Request.URL, r.StatusCode)
 	})
 
-	c.OnError(func(r *colly.Response, err error) {
-		log.Println("Request URL:", r.Request.URL, "\nError:", err)
-		// 認証オプションの使用を勧める
-	})
+	// c.OnError(func(r *colly.Response, err error) {
+	// 	log.Println("Request URL:", r.Request.URL, "\nError:", err)
+	// 	// unautorizedなら、認証オプションの使用を勧める
+	// })
+
+	// リクエスト間隔（Delay）
+	// c.Limit(&colly.LimitRule{
+	// 	DomainGlob: "*",
+	// 	Delay:      10 * time.Second,
+	// })
 
 	c.Request("GET", args[0], nil, nil, hdr)
 }
